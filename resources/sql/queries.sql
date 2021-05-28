86da433957fbd9052a738129073b7b25be4a46a9
Jacob Vigeveno <304282+vigevenoj@users.noreply.github.com>
2021-05-27 22:11:19 -0700

    add previous/next to single photo query

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -40,15 +40,18 @@ WHERE id = :id
 
 -- :name get-dog-photo-by-uuid :? :1
 -- :doc retrieve a dog photo given the uuid
-SELECT id, name, taken, metadata FROM photos
-WHERE name = :name;
+SELECT p.id, p.name, p.taken, p.metadata,
+(select name from photos where taken < p.taken order by taken desc limit 1) as previous,
+(select name from photos where taken > p.taken order by taken asc limit 1) as next
+FROM photos p
+WHERE name = :name
 
 -- :name get-previous-years-photos :? :*
 -- :doc retrieve dog photos from this day in previous years
 SELECT id, name, taken, metadata from photos
 WHERE extract(month from taken) = :month
 AND extract(day from taken) = :day
-ORDER BY taken desc;
+ORDER BY taken desc
 
 -- :name get-recent-photos :? :*
 -- :doc retrieve recently-taken dog photos
@@ -56,3 +59,47 @@ SELECT id, name, taken, metadata
 FROM photos
 ORDER BY taken DESC
 LIMIT :limit
+
+-- :name get-photos-by-date :? :*
+-- :doc retrieve dog photos from a given date
+SELECT id, name, taken, metadata
+FROM photos
+WHERE taken = :taken
+
+-- :name get-photo-by-metadata :? :*
+-- :doc retrieve photos with matching metadata
+SELECT id, name, taken, metadata
+FROM photos
+WHERE metadata = :metadata
+
+-- :name get-next-more-recent-by-timestamp :? :1
+-- :doc retrieve the photo one newer than the given timestamp
+SELECT id, name, taken, metadata
+FROM photos
+WHERE taken > :taken
+ORDER BY taken asc
+LIMIT 1
+
+-- :name get-next-older-by-timestamp :? :1
+-- :doc retrieve the photo one older than the given timestamp
+SELECT id, name, taken, metadata
+FROM photos
+WHERE taken < :taken
+ORDER BY taken desc
+LIMIT 1
+
+-- :name get-next-more-recent-photo :? :1
+-- :doc get the photo one newer than the given uuid
+SELECT id, name, taken, metadata
+FROM photos
+WHERE taken > (select taken from photos where name = :name)
+ORDER BY taken asc
+LIMIT 1
+
+-- :name get-next-older-photo :? :1
+-- :doc get the photo one older than the given uuid
+SELECT id, name, taken, metadata
+FROM photos
+WHERE taken < (select taken from photos where name = :name)
+ORDER BY taken desc
+LIMIT 1
